07fbe1145efb7a4f43ad8c6fdaf255b070cd7dbc
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-07 16:55:52 +0100

    Added function deployServer that deploys a server when invoked

---
+++ src/server/deployServer.js
@@ -0,0 +1,16 @@
+const debug = require("debug")("pym:server");
+const chalk = require("chalk");
+
+const deployServer = (port, app) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.green(`Server is up at http://localhost:${port}`));
+      resolve();
+    });
+
+    server.on("error", (error) => {
+      reject(new Error(`Error on server: ${error.message}`));
+    });
+  });
+
+module.exports = deployServer;
