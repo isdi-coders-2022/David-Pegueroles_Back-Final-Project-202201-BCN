ec0160a6667d5626a4ab142a39b4d58b6de2deb8
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-10 19:00:44 +0100

    Added tests for the generalError middleware function

--- src/server/middlewares/errors.test.js
+++ src/server/middlewares/errors.test.js
@@ -1,4 +1,4 @@
-const { notFoundError } = require("./errors");
+const { notFoundError, generalError } = require("./errors");
 
 describe("Given a notFoundError handler", () => {
   describe("When it receives a response", () => {
@@ -15,6 +15,44 @@ describe("Given a notFoundError handler", () => {
 
       expect(res.status).toHaveBeenCalledWith(status);
       expect(res.json).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given a generalError handler", () => {
+  describe("When it receives an empty error and a response", () => {
+    test("Then it should call res methods status with 500 status code & json with an error and the message 'General Error'", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const err = {};
+
+      const errorMessage = { error: true, message: "General Error" };
+      const status = 500;
+
+      await generalError(err, null, res, null);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+      expect(res.json).toHaveBeenCalledWith(errorMessage);
+    });
+  });
+
+  describe("When it receives an error with properties code: 401 & message: 'This is an error' and a response", () => {
+    test("Then it should call methods status with the received status code & json with the received error", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const err = { code: 401, message: "This is an error" };
+
+      const errorMessage = { error: true, message: "This is an error" };
+      const status = 401;
+
+      await generalError(err, null, res, null);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+      expect(res.json).toHaveBeenCalledWith(errorMessage);
     });
   });
 });
