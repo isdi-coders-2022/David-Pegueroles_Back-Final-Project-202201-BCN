1b33b0648e70df0c5cae5b9415407df51781eacb
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-10 19:47:29 +0100

    Added call to the databaseConnection function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 const debug = require("debug")("pym:root");
 const chalk = require("chalk");
+const databaseConnection = require("./database");
 const app = require("./server");
 const deployServer = require("./server/deployServer");
 
 const port = process.env.PORT || 6254;
+const database = process.env.PIN_YOUR_MEETINGS_DB;
 
 (async () => {
   try {
     await deployServer(port, app);
+    await databaseConnection(database);
   } catch (error) {
     debug(chalk.bgRed.white(error.message));
   }
