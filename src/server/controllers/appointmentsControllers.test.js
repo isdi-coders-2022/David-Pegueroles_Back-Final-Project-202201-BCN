7e276136b8ad3223956da53689aa3998b8510d0f
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-04-01 09:19:48 +0200

    Added test of the createAppointment controller function

--- src/server/controllers/appointmentsControllers.test.js
+++ src/server/controllers/appointmentsControllers.test.js
@@ -3,6 +3,7 @@ const {
   getDailyAppointments,
   getAppointmentInfo,
   deleteAppointment,
+  createAppointment,
 } = require("./appointmentsControllers");
 
 jest.mock("../../database/models/Appointment");
@@ -184,6 +185,82 @@ describe("Given a deleteAppointment controller", () => {
       Appointment.findByIdAndDelete = jest.fn().mockRejectedValueOnce(error);
 
       await deleteAppointment(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given a createAppointment controller", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call res status and json methods with a 200 and the created appointment respectively", async () => {
+      const newAppointment = {
+        name: "Do something else",
+        description: "This is another thing to do",
+        date: "2022-03-31",
+        category: "Work",
+        location: "C/ Diputació 37, Barcelona",
+        hour: "12:00",
+        id: "1",
+      };
+      const req = {
+        body: newAppointment,
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const status = 201;
+
+      Appointment.create = jest.fn().mockReturnValue(newAppointment);
+
+      await createAppointment(req, res);
+
+      expect(Appointment.create).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(status);
+      expect(res.json).toHaveBeenCalledWith({ newAppointment });
+    });
+  });
+
+  describe("When it receives a non valid request", () => {
+    test("Then it should call res status and json methods with a 200 and the created appointment respectively", async () => {
+      const newAppointment = {
+        name: "Do something else",
+        description: "This is another thing to do",
+        date: "2022-03-31",
+        nonExistingProperty: "true",
+      };
+      const req = {
+        body: newAppointment,
+      };
+
+      const next = jest.fn();
+      const error = new Error("Could not create the appointment");
+
+      Appointment.create = jest.fn().mockReturnValue(null);
+
+      await createAppointment(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("When an error occurs", () => {
+    test("Then it should call next with an error with status 400", async () => {
+      const req = {
+        params: {
+          idAppointment: "1",
+        },
+      };
+      const res = null;
+      const next = jest.fn();
+      const error = {
+        status: 400,
+      };
+
+      Appointment.create = jest.fn().mockRejectedValueOnce(error);
+
+      await createAppointment(req, res, next);
 
       expect(next).toHaveBeenCalledWith(error);
     });
